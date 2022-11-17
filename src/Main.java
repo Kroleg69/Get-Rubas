29531336b80d9ea25dc7f896c121482754261007
Oleg Sidorov <sidoroffoleg@mail.ru>
2022-11-17 20:49:53 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int add = 1100;
+
+        int bonus = 0;
+        if (add > 1000) {
+            bonus = add / 100;
+        }
+
+        int finalAccount = account + add + bonus;
+
+        System.out.println("Итоговый счет:" + finalAccount);
+        System.out.println("Бонус:" + bonus);
+
+    }
+
+}
+
